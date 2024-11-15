56317e05743844aae983522387f6981c7a8278ce
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2024-11-15 11:32:23 -0400

    fix: could not click on exercicio name input

--- MuscleTrackApp/src/main/java/muscletrack/app/components/TreinoForm.java
+++ MuscleTrackApp/src/main/java/muscletrack/app/components/TreinoForm.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class TreinoForm {
-    private VBox treinoFormBox;
+    private final VBox treinoFormBox;
 
     public TreinoForm(){
         try {
